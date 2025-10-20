2f0c8a9291ceac9cf8fde83680a4a45ccf94deba
Your Name <you@example.com>
2025-10-20 15:48:29 +0100

    add npm publish workflow

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const inquirer = require("inquirer");
+const inquirer = require("inquirer"); 
 const fs = require("fs-extra");
 const path = require("path");
 const chalk = require("chalk");
